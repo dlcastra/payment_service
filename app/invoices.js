0abf8318e7664d909073f64e4e87be7c7e7bfc60
dlcastra <1dlcastra@gmail.com>
2024-10-12 15:09:21 +0200

    webhook update. Dynamic change of transaction status.

--- app/invoices.js
+++ app/invoices.js
@@ -10,6 +10,7 @@ const {
     createInvoice,
     createTransaction
 } = require("../app/utils");
+const {Transaction} = require("../models/transaction");
 
 const router = express.Router();
 
@@ -17,15 +18,11 @@ router.post("/make-transaction", async (req, res) => {
     const transactionId = uuidv4();
     const baseUrl = `${req.protocol}://${req.get('host')}`;
     const webhookUrl = `${baseUrl}/webhook`;
-    console.log(req.body)
-    console.log(transactionId)
-    console.log(webhookUrl)
 
     const invoiceUrl = await createInvoice(req, transactionId, webhookUrl)
     if (!invoiceUrl) {
         return res.status(500).json({error: "Invoice creation failed."});
     }
-    console.log(invoiceUrl)
 
     const transaction = await createTransaction(req, transactionId, invoiceUrl)
     if (!transaction) {
@@ -39,23 +36,31 @@ router.post("/make-transaction", async (req, res) => {
 router.post("/webhook", async (req, res) => {
     const publicKey = await getMonobankPublicKey();
 
+    // Check signature
     try {
         const xSign = req.headers["x-sign"];
         if (!xSign) {
             return res.status(400).json({error: "Header is missing"});
         }
-
         const isValid = verifySignature(req, publicKey, xSign);
         if (isValid) {
             console.log("Signature is valid");
         } else {
             res.status(400).json({error: "Invalid signature"})
         }
-
     } catch (error) {
         console.error("Error verifying signature:", error.message);
         res.status(400).json({error: error.message});
     }
+
+    // Transaction status
+    const {reference} = req.body;
+    const {status} = req.body
+    const transaction = await Transaction.findOne({where: {transactionId: reference} });
+    transaction.transactionStatus = status
+    await transaction.save()
+
+    console.info(`Status of the transaction with id "${reference}" has been changed to "${status}"`)
 });
 
 module.exports = router;
